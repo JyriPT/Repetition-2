ea510d49e1f22dffc41abfe41c3611f74671b812
Jyri Turku <jyri.turku@student.saimia.fi>
2019-09-18 12:50:42 +0300

    Now calculates square roots.

--- NumberFun/NumberFun/Program.cs
+++ NumberFun/NumberFun/Program.cs
@@ -6,7 +6,45 @@ namespace NumberFun
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int select = 0;
+            Console.WriteLine("Select the assignment you wish to check (number 1-6):");
+            Console.WriteLine("");
+
+            select = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+
+            if (select == 1)
+            {
+                Squares();
+            } else if (select == 2)
+            {
+                Console.WriteLine("This assignment is being worked on.");
+            } else if (select == 3)
+            {
+                Console.WriteLine("This assignment is being worked on.");
+            } else if (select == 4)
+            {
+                Console.WriteLine("This assignment is being worked on.");
+            } else if (select == 5)
+            {
+                Console.WriteLine("This assignment is being worked on.");
+            } else if (select == 6)
+            {
+                Console.WriteLine("This assignment is being worked on.");
+            } else
+            {
+                Console.WriteLine("Invalid selection, please reboot.");
+            }
+        }
+
+        static void Squares()
+        {
+            Console.WriteLine("Luku         Neliöjuuri");
+            
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i + "         " + Math.Round(Math.Sqrt(i), 2));
+            }
         }
     }
 }
